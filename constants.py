a8f0093e9edf421e9f247a5e73c71b414e85a3ad
zatchet <zach@rippas.org>
2024-11-22 20:44:24 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-LIGHT_RELEASE_RATE = 1 # when the light is green, cars go through it every LIGHT_RELEASE_RATE seconds
+LIGHT_RELEASE_RATE = 1 # when the light turns green, cars that were in queue go through it every LIGHT_RELEASE_RATE seconds
 STOP_SIGN_RELEASE_RATE = 2 # every STOP_SIGN_RELEASE_RATE seconds, a car is released from the stop sign
 LEFT_TURN_PENALTY = 0.5 # when a car turns left, it has to wait an additional LEFT_TURN_PENALTY seconds
 MOVEMENT_DELAY = 0.5 # time it takes a car to move one cell (e.g. through a green light)
